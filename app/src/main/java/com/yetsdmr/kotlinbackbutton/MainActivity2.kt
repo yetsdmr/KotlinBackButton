13890e6cdc8260cf191bb3e819a4b46a979dcd53
yetsdmr <yetsdmr@gmail.com>
2020-09-11 10:29:50 +0300

    Initial Commit

---
+++ app/src/main/java/com/yetsdmr/kotlinbackbutton/MainActivity2.kt
@@ -0,0 +1,27 @@
+package com.yetsdmr.kotlinbackbutton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home ->{
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+    }
+}
